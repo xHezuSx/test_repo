4618fa12535d6f52e9c11e5c34bf44c487f89d90
xHezuSx <jakubek.zakowski@gmail.com>
2023-12-06 15:50:01 +0100

    zmiany123

---
+++ klasa.py
@@ -0,0 +1,7 @@
+class Dodaj:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def f(self):
+        return self.x + self.y
--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 print("twoja jebana stara")
 print(15*100, "kochani kocham was! "*2)
+
+from klasa import Dodaj
+
+obj = Dodaj(10, 3)
+print(obj.f())
